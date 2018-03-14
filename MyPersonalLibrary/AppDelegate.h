f4ebd3710e5f2401b49cd22f85339fe9ffbb2cce
Howeix <495658492@qq.com>
2018-03-14 17:57:34 +0800

    Initial Commit

---
+++ MyPersonalLibrary/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MyPersonalLibrary
+//
+//  Created by 黄炜 on 2018/3/14.
+//  Copyright © 2018年 黄炜. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
